0d52a0883f1b2228f87bf2446c1e2bbce12be9ea
Elvis <orest.kupchak@gmail.com>
2017-08-03 01:31:29 +0300

    Added comments

--- O-YI_dropdown/app.js
+++ O-YI_dropdown/app.js
@@ -1,5 +1,5 @@
 
-function makeUL(array) {
+function makeUl(array) {
     // Create the list element:
     var list = document.createElement('ul');
     list.setAttribute("id", "city_list");
@@ -18,13 +18,13 @@ if (array[i].population / 1000000 >= 1) {
         // Add it to the list:
         list.appendChild(item);
     }
-    // Finally, return the constructed list:
+    //  Return the constructed list:
     return list;
 }
 
 
 
-
+//Create a search field
 var search = document.createElement("INPUT");
 search.setAttribute("type", "search");
 search.setAttribute("id", "search_field");
@@ -33,20 +33,20 @@ search.setAttribute("onkeyup", "find()");
 
 document.getElementById('dropdown').appendChild(search);
 
-document.getElementById('dropdown').appendChild(makeUL(LIST));
+document.getElementById('dropdown').appendChild(makeUl(LIST));
 
-
+//Show/hide menu on click
 document.getElementsByClassName('menu_btn')[0].addEventListener('click',toggle)
 
-function toggle(){
+function toggle(){//Change classes of elem to show/hide
   var nav = document.getElementById('dropdown');
   var btn = document.getElementsByClassName('menu_btn')[0];
   nav.classList.toggle('open');
-  
   btn.classList.toggle('reverse');
 
 }
 
+//Set the text of selected element of the list into the button
 document.getElementsByTagName('ul')[0].addEventListener('click', function (event){
   var selected = event.target;
   if (selected.tagName === 'LI') {
@@ -55,13 +55,14 @@ document.getElementsByTagName('ul')[0].addEventListener('click', function (event
 }
 })
 
+//Higlight the selected elem
 function highlite(node){
       var selected = event.target;
            unselectAll()
      selected.classList.toggle('selected');
 
       };
-
+//and only that one elem, others are autmatically unselected
     function unselectAll() {
       var ul = document.getElementsByTagName('ul')[0]
       for (var i = 0; i < ul.children.length; i++) {
@@ -69,7 +70,7 @@ function highlite(node){
       }
     }
 
-
+//Live search 
 function find() {
 
     var input, filter, ul, li;
@@ -80,7 +81,7 @@ function find() {
 
     // Loop through all list items, and hide those who don't match the search query
     for ( var i = 0; i < li.length; i++) {
-    if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+    if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) { 
         li[i].style.display = "";
     } else {
         li[i].style.display = "none";
